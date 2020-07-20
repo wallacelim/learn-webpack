e2c0003ac274e17b5ae93d6c666ba5410bb9453c
Wallace Lim <wallace.lim@shopee.com>
2020-07-20 16:27:11 +0800

    Loading images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import "./style.css";
+import MyIcon from "./icon.png";
 
 function component() {
   const element = document.createElement("div");
@@ -7,6 +8,11 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
+
+  const myIcon = new Image(400);
+  myIcon.src = MyIcon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
