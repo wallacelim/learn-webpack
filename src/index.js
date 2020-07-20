c8b92beb1b2d2adbc2d256b873a4000fc2b3b135
Wallace Lim <wallace.lim@shopee.com>
2020-07-20 16:48:18 +0800

    Add html-webpack-plugin + clean-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 import _ from "lodash";
+
 import "./style.css";
 import MyIcon from "./icon.png";
 import MyData from "./data.xml";
 // NOTE: only default exports from JSON files can be used
+import printMe from "./print";
 
 function component() {
   const element = document.createElement("div");
-
-  // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  // Example for loading styles
   element.classList.add("hello");
 
+  // Example for loading images
   const myIcon = new Image(400);
   myIcon.src = MyIcon;
 
-  element.appendChild(myIcon);
-
+  // Example for loading data
   console.log(MyData);
+
+  // Example for output management
+  const button = document.createElement("button");
+  button.innerHTML = "Click me and then check the console!";
+  button.onclick = printMe;
+
+  element.appendChild(button);
+  element.appendChild(myIcon);
 
   return element;
 }
