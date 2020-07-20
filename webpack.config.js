378133c4dce9c529ecaaa63933b3e3a85d95a171
Wallace Lim <wallace.lim@shopee.com>
2020-07-20 16:10:00 +0800

    Setup config for style + css loading

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,15 @@ module.exports = {
   // Entry point for the webpack bundler to build its internal dependency graph
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
